fb7f16518137cff1d93fcd2177aef4fff7670497
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-06-21 17:54:15 +0300

    create typings for typescript

---
+++ typings/dataclass.d.ts
@@ -0,0 +1,8 @@
+declare module 'dataclass' {
+  // https://github.com/Microsoft/TypeScript/issues/5863
+  export default class Record<T> {
+    constructor(custom?: Partial<T>);
+    copy(patch: Partial<T>): T;
+    equals(record: T): boolean;
+  }
+}
