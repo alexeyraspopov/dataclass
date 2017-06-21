3035bf72c504a831d53148780dc4d65f57330712
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-06-21 19:10:07 +0300

    create tests for Record

---
+++ modules/__tests__/Record-test.js
@@ -0,0 +1,44 @@
+/* @flow */
+import Record from '../Record';
+
+describe('Record', () => {
+  class Entity extends Record {
+    someString: string = 'default string';
+    someNum: number = 0.134;
+    someBool: boolean = true;
+    someNullable: ?string = null;
+  }
+
+  it('should create an entity with default values', () => {
+    const entity = new Entity();
+
+    expect(entity)
+      .toEqual({ someString: 'default string', someNum: 0.134,
+                 someBool: true, someNullable: null });
+  });
+
+  it('should override defaults with custom values', () => {
+    const entity = new Entity({ someNullable: 1, someString: 'hello' });
+
+    expect(entity)
+      .toEqual({ someString: 'hello', someNum: 0.134,
+                 someBool: true, someNullable: 1 });
+  });
+
+  it('should create new entity based on existent', () => {
+    const entity = new Entity({ someBool: false });
+    const updated = entity.copy({ someNum: 14 });
+
+    expect(entity)
+      .toEqual({ someString: 'default string', someNum: 0.134,
+                 someBool: false, someNullable: null });
+
+    expect(updated)
+      .toEqual({ someString: 'default string', someNum: 14,
+                 someBool: false, someNullable: null });
+  });
+
+  it('should compare custom values for two entities of the same type', () => {
+
+  });
+});
