c51622a6711d0cd7b9dd12b03d221882afda263a
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-06-23 22:12:22 +0300

    update comment

--- typings/dataclass.d.ts
+++ typings/dataclass.d.ts
@@ -1,4 +1,5 @@
 declare module 'dataclass' {
+  // Record is generic due to the issue with constructor typings
   // https://github.com/Microsoft/TypeScript/issues/5863
   export default class Record<T> {
     constructor(custom?: Partial<T>);
