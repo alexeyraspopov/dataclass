b649563843797e69168c3f777aef3d18a21ec448
Alexey Raspopov <alexey.raspopov@datarobot.com>
2021-10-07 13:16:38 -0400

    inline static helper

--- typings/dataclass.d.ts
+++ typings/dataclass.d.ts
@@ -1,8 +1,6 @@
 export class Data {
-  static create<Type extends Data>(this: Static<Type>, custom?: Partial<Type>): Type;
+  static create<Type extends Data>(this: { new (): Type }, custom?: Partial<Type>): Type;
   copy(patch: Partial<this>): this;
   equals(record: this): boolean;
   toJSON(): Object;
 }
-
-type Static<Type extends Data> = { new (): Type };
