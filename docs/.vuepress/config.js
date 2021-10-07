8f5ceb268293d1affb1d5bb28a60d57e72ad3131
Alexey Raspopov <alexey.raspopov@datarobot.com>
2021-10-07 17:44:20 -0400

    use vuepress for the new docs website

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  title: 'dataclass',
+
+  head: [
+    ['meta', { name: 'theme-color', content: '#4770eb' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' }],
+  ],
+
+  themeConfig: {
+    repo: 'alexeyraspopov/dataclass',
+    docsBranch: 'master',
+    docsDir: 'docs',
+
+    navbar: [
+      {
+        text: 'Guide',
+        link: '/guide/',
+      },
+      {
+        text: 'API Reference',
+        link: '/reference/api.html',
+      },
+    ],
+
+    sidebar: {
+      '/guide/': [
+        {
+          text: 'Guide',
+          children: [
+            '/guide/index.md',
+            '/guide/getting-started.md',
+            '/guide/serialization-deserialization.md',
+            '/guide/contributing.md',
+          ],
+        },
+      ],
+      '/reference/': [
+        {
+          text: 'Reference',
+          children: ['/reference/api.md'],
+        },
+      ],
+    },
+  },
+
+  bundler: '@vuepress/vite',
+  plugins: ['@vuepress/plugin-search'],
+};
---
+++ docs/index.md
@@ -0,0 +1,29 @@
+---
+home: true
+tagline: Data Classes for TypeScript & JavaScript
+actions:
+  - text: Get Started
+    link: /guide/
+    type: primary
+  - text: API Reference
+    link: /reference/api.html
+    type: secondary
+  - text: GitHub
+    link: https://github.com/alexeyraspopov/dataclass
+    type: secondary
+features:
+  - title: Lightweight
+    details:
+      The library is less than 500B after build min+gzip while still providing a lot of flexibility
+      and typings
+  - title: Immutable
+    details: The power of value objects is based on the convention that objects do not mutate
+  - title: Delightful
+    details:
+      The library is built with developer experience in mind. Things should be easy to implement and
+      the tools should provide meaningful feedback
+footer:
+  Made by <a href="https://twitter.com/alexeyraspopov" rel="noopener noreferrer"
+  target="_blank">Alexey Raspopov</a> with ❤️
+footerHtml: true
+---
