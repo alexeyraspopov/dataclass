68def1ebac275f8ed7bd8b0bc7d14e121bc7ea0e
Alexey Raspopov <alexeyraspopov@hotmail.com>
2022-09-26 09:40:10 -0400

    run modern tests using vitests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
 	collectCoverage: true,
+	testMatch: ["**/__tests__/*-test.js"],
 	transform: {
 		"\\.js$": ["babel-jest", { presets: ["@babel/preset-env", "@babel/preset-flow"] }],
 	},
