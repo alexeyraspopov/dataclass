33741f1be6dcab984081c56f05bc21a65aa122c6
Alexey Raspopov <alexeyraspopov@hotmail.com>
2022-09-26 09:39:21 -0400

    allow empty copy arg

--- typings/dataclass.d.ts
+++ typings/dataclass.d.ts
@@ -1,5 +1,5 @@
 export class Data {
   static create<Type extends Data>(this: { new (): Type }, values?: Partial<Type>): Type;
-  copy(values: Partial<this>): this;
+  copy(values?: Partial<this>): this;
   equals(other: this): boolean;
 }
