5fd04e8addfb5158e449c422a0336e410c83630a
Alexey Raspopov <alexeyraspopov@hotmail.com>
2022-09-26 09:37:09 -0400

    generate readme from rollup

--- rollup.config.js
+++ rollup.config.js
@@ -9,7 +9,7 @@ export default {
       targets: [
         { src: "modules/Data.js", dest: "build", rename: "dataclass.module.js" },
         { src: ["typings/*", "LICENSE"], dest: "build" },
-        { src: "README_NPM.md", dest: "build", rename: "README.md" },
+        { src: "README.md", dest: "build", transform: generateReadme },
         { src: "package.json", dest: "build", transform: generatePkg },
       ],
     }),
@@ -38,4 +38,36 @@ function generatePkg(contents) {
     null,
     2,
   );
+}
+
+function generateReadme() {
+  return `
+The library brings flexibility and usefulness of data classes from Kotlin, Scala, or Python to TypeScript and JavaScript.
+
+Read full docs [on the homepage](https://dataclass.js.org).
+
+\`\`\`javascript
+import { Data } from "dataclass";
+
+// 1. easily describe your data classes using just language features
+class User extends Data {
+  name: string = "Anon";
+  age: number = 0;
+}
+
+// 2. instantiate classes while type systems ensure correctness
+let user = User.create({ name: "Liza", age: 23 });
+// > User { name: "Liza", age: 23 }
+
+// 3. make changes while benefiting from immutable values
+let updated = user.copy({ name: "Ann" });
+// > User { name: "Ann", age: 23 }
+updated = updated.copy({ name: "Liza" });
+// > User { name: "Liza", age: 23 }
+
+// 4. compare objects by their value, not reference
+console.log(user === updated, user.equals(updated));
+// > false, true
+\`\`\`
+  `.trim();
 }
