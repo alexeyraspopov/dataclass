367fa14bb8227bd0c0145990f849efaf79ffcb8e
Alexey Raspopov <alexeyraspopov@hotmail.com>
2022-11-08 19:52:24 -0500

    omit base keys in method signatures

--- typings/dataclass.d.ts
+++ typings/dataclass.d.ts
@@ -1,5 +1,8 @@
 export class Data {
-  static create<Type extends Data>(this: { new (): Type }, values?: Partial<Type>): Type;
-  copy(values?: Partial<this>): this;
+  static create<Type extends Data>(
+    this: { new (): Type },
+    values?: Omit<Partial<Type>, keyof Data>,
+  ): Type;
+  copy(values?: Omit<Partial<this>, keyof Data>): this;
   equals(other: this): boolean;
 }
